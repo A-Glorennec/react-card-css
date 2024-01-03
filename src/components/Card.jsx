e10e3e778216a6d0e3e3e210030f1cf45d60630c
ANAIS GLORENNEC <zans_69@hotmail.fr>
2024-01-03 09:52:17 +0100

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Card.scss";
+
+const Card = () => {
+    return (
+        <article>
+            <img src="" alt="" />
+            <h1>Title</h1>
+            <div>
+               <i class="fa-regular fa-star"></i>
+            </div>
+        </article>
+    );
+}
+
+export default Card;
